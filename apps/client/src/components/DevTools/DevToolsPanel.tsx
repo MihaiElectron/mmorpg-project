e0e9db74b174435e091de153d8acf2fbd5c1ed8f
Mihai Radulescu <mihaielectron@gmail.com>
2026-06-23 21:28:44 +0200

    refactor(studio): restaurer AdminPanel legacy comme interface principale DevTools
    - DevToolsPanel réduit à WorldModule + OverlayControls + AdminPanel
    - modules WOM (Resources, Animals, CreatureSpawns, Inspector, Actions, CommandPalette, Validation) conservés dans le code mais retirés du rendu
    - ergonomie opérationnelle immédiate : templates, instances, drag-drop, suppression

--- apps/client/src/components/DevTools/DevToolsPanel.tsx
+++ apps/client/src/components/DevTools/DevToolsPanel.tsx
@@ -1,26 +1,12 @@
 import AdminPanel from "../AdminPanel/AdminPanel";
 import { WorldModule } from "./modules/World";
-import { ResourcesModule } from "./modules/Resources";
-import { AnimalsModule } from "./modules/Animals";
-import { CreatureSpawnsModule } from "./modules/CreatureSpawns";
-import WorldObjectInspector from "./WorldObjectInspector";
-import ValidationPanel from "./ValidationPanel";
 import OverlayControls from "./OverlayControls";
-import SelectedActionsPanel from "./SelectedActionsPanel";
-import CommandPalette from "./CommandPalette";
 
 export default function DevToolsPanel() {
   return (
     <>
       <WorldModule />
       <OverlayControls />
-      <ResourcesModule />
-      <AnimalsModule />
-      <CreatureSpawnsModule />
-      <WorldObjectInspector />
-      <SelectedActionsPanel />
-      <CommandPalette />
-      <ValidationPanel />
       <AdminPanel />
     </>
   );
